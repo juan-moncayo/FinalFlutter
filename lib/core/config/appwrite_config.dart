7eb2946c85b3e849735fc51e26ef16ca96681c19
juan-moncayo <juan.moncayo627@gmail.com>
2025-05-16 12:45:24 -0500

    configuracion appwrite y credenciales

--- lib/core/config/appwrite_config.dart
+++ lib/core/config/appwrite_config.dart
@@ -2,16 +2,16 @@ import 'package:appwrite/appwrite.dart';
 import 'package:placas_app/core/constants/appwrite_constants.dart';
 
 class AppwriteConfig {
-  static Client client = Client();
+  static late Client client;
   static late Account account;
   static late Databases databases;
   static late Storage storage;
 
   static Future<void> init() async {
-    client
-        .setEndpoint(AppwriteConstants.endpoint)
-        .setProject(AppwriteConstants.projectId)
-        .setSelfSigned(status: true); // Solo para desarrollo
+    client = Client()
+      ..setEndpoint(AppwriteConstants.endpoint)
+      ..setProject(AppwriteConstants.projectId)
+      ..setSelfSigned(status: true);
 
     account = Account(client);
     databases = Databases(client);
