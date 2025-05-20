c40926d3bbaa2faa365ce4aa919ab5497c9f9903
juan-moncayo <juan.moncayo627@gmail.com>
2025-05-19 19:57:01 -0500

    varibles y main

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ESP32 Monitor',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
